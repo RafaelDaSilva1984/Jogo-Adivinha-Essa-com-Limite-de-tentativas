ebe5cff7526f1c35bd3618ef47c6f971a1d93911
Rafael_Silva_1984 <101530186+RafaelDaSilva1984@users.noreply.github.com>
2023-07-13 21:15:42 -0300

    Update3 Jogo_adivinha_essa.py

--- Adivinha essa Python 007/Jogo_adivinha_essa.py
+++ Adivinha essa Python 007/Jogo_adivinha_essa.py
@@ -3,24 +3,25 @@ print('Qual número estou pensando entre 0 a 10!!!!')
 comp = 8
 max = 3
 tentativas = 1
-cont = 0
+cont = 1
 while tentativas <= max:
     
   jogador = int(input('Tente adivinhar: ')) # o input tem que ser depois do while para não ficar se repetindo   
   if jogador == comp:
     print('Parabéns você adivinhou... eu escondi o número:',comp)
+    print(f'Acertou na tentativa {cont }')
     break # para o programa após acertar o número do computador
   elif jogador > comp:
     print('Número escondido é Menor...')
     cont += 1
     print(f'Testativa restante {3 - cont }')
   elif jogador < comp:
-    print('Número escondido é Maior...')
-    cont += 1
+    print('Número escondido é Maior...')    
     print(f'Testativa restante {3 - cont}')
   if tentativas == max:
     print('Você perdeu meu número escondido é: ',comp)
-    cont += 1
+    
   tentativas += 1 # somas o número de tentativas até o max indicado
+  cont += 1
     
   
